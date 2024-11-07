b5d308c95994904327a38ceded2b9fdcaa353008
OlyaSochneva <136125155+OlyaSochneva@users.noreply.github.com>
2024-11-07 23:49:11 +0400

    Update test_get_categories.py

--- tests/categories/test_get_categories.py
+++ tests/categories/test_get_categories.py
@@ -27,31 +27,6 @@ class TestGetCategories:
 
 
 
-    #@allure.title('По запросу GET /api/categories/ категориям 1 уровня соответствует правильный список '
-                  #'подкатегорий (проверяем по названиям)')
-    #@pytest.mark.parametrize("category_name, sample", [
-        #("Транспорт", Data.TRANSPORT_SUBCATEGORIES),
-        #("Услуги", Data.SERVICES_SUBCATEGORIES),
-        #("Строительство", Data.CONSTRUCTION_SUBCATEGORIES)
-    #])
-    #def test_second_level_categories(self, category_name, sample):
-        #response = requests.get(URL.CATEGORIES, params={'limit': 100}, timeout=10)
-        #result = check_subcategories_names(response.json(), category_name, sample)
-        #assert response.status_code == 200 and result == "Correct"
-
-    #@allure.title('По запросу GET /api/categories/ категориям 2 уровня соответствует правильный список '
-                  #'подкатегорий (проверяем по названиям)')
-    #@pytest.mark.parametrize("category_name, sample", [
-        #("Ремонт и обслуживание техники", Data.REPAIR_SERVICE_SUBCATEGORIES),
-        #("Красота и здоровье", Data.BEAUTY_HEALTH_SUBCATEGORIES),
-        #("Обучение и курсы", Data.STUDYING_SUBCATEGORIES),
-        #("Аренда техники", Data.TRANSPORT_RENTAL_SUBCATEGORIES),
-        #("Пассажирские перевозки", Data.TRANSPORTATION_SUBCATEGORIES)
-    #])
-    #def test_third_level_categories(self, category_name, sample):
-        #response = requests.get(URL.CATEGORIES, params={'limit': 100}, timeout=10)
-        #result = check_subcategories_names(response.json(), category_name, sample)
-        #assert response.status_code == 200 and result == "Correct"
 
 
 
